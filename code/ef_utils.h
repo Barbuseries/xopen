0cf62f5f111381a83b2bbe8358d7441d723f1ca5
Edorh François <francois.edorh@isen-lille.fr>
2016-12-19 02:44:25 +0100

    Add instruction syntax and execution.
    - hello, world
    - Config file is parsed to find instruction associated with file's
      extension.
    - A command operate on all files with the same extension.
    - Can display command instead of executing it.

---
+++ code/ef_utils.h
@@ -0,0 +1,177 @@
+#ifndef EF_UTILS_H
+#define EF_UTILS_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Types
+typedef int8_t		i8;
+typedef uint8_t		u8;
+
+typedef int16_t		i16;
+typedef uint16_t	u16;
+
+typedef int32_t		i32;
+typedef uint32_t	u32;
+
+typedef int64_t		i64;
+typedef uint64_t	u64;
+
+typedef float		r32;
+typedef double		r64;
+
+typedef int			b32;
+
+// Utilities
+#define JOIN_HELPER(x, y) x ## y
+#define JOIN(x, y)        JOIN_HELPER(x, y)
+#define JOIN3(x, y, z)    JOIN(JOIN(x, y), z)
+
+#define STRINGIFY(x)    #x
+#define TO_STRING(x)	STRINGIFY(x)
+
+#define SWAP(type, a, b) do						\
+	{											\
+		type	ef__temp = a;					\
+		a				 = b;					\
+		b				 = ef__temp;			\
+	}while (0)
+
+#define CLAMP(x, min, max) ((x) < (min)) ? (min) : (x) > (max) ? (max) : (x)
+#define MIN(a, b) ((a) < (b)) ? (a) : (b)
+#define MAX(a, b) ((a) > (b)) ? (a) : (b)
+
+#define OFFSET_OF(type, f) (size_t) &((type *)0)->f
+
+// Array
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+#define INDEX_OF(it, arr)	((size_t) ((it) - (arr)))
+
+#define FOR_EACH(type, it, arr) for(type* it = arr;	INDEX_OF(it, arr) < ARRAY_SIZE(arr); ++it)
+#define FOR_EACH_IT(type, arr)  FOR_EACH(type, it, arr)
+
+#define FOR_INDEX(index, arr) for(size_t index = 0; index < ARRAY_SIZE(arr); ++index)
+#define FOR_I(arr)	          FOR_INDEX(i, arr)
+#define FOR_J(arr)	          FOR_INDEX(j, arr)
+
+// TODO: Allow pointers to be used aswell (replace FOR_INDEX).
+#define PRINT_N_ARRAY(format, interFormat, arr, n) do	\
+	{													\
+		FOR_INDEX(ef__prarr_i, arr)						\
+		{												\
+			printf(format, arr[ef__prarr_i]);			\
+														\
+			if (ef__prarr_i < (size_t) (n - 1))			\
+			{											\
+				printf("%s", interFormat);				\
+			}											\
+			else										\
+			{											\
+				break;									\
+			}											\
+		}												\
+														\
+	}while (0)
+
+#define PRINT_ARRAY(format, interformat, arr) PRINT_N_ARRAY(format, interformat, arr, ARRAY_SIZE(arr))
+
+
+// TODO: Change both functions below to be in O(n).
+
+// NOTE: Copy src rotated rotationCount times to the right into dest.
+//       If rotationCount is 0, just copy src into dest (if they are not the same).
+// IMPORTANT: Do not forget to either use strlen or to count one less
+// when calling this on a nul-terminated string.
+void rotateRightArray(void *dest, void *src, size_t itemSize, size_t itemCount,
+					  size_t rotationCount = 1)
+{
+	if (rotationCount >= itemCount)
+	{
+		rotationCount %= itemCount;
+	}
+
+	if ((rotationCount == 0) && (src != dest))
+	{
+		memcpy(dest, src, itemSize * itemCount);
+	}
+
+	if (rotationCount > 0)
+	{	
+		u8 tmp[itemSize];
+		memcpy(tmp, src + itemSize * (itemCount - 1), 1);
+
+		memcpy(dest + itemSize, src, itemSize * (itemCount - 1));
+		memcpy(dest, tmp, 1);
+		--rotationCount;
+	}
+
+	src = dest;
+	while (rotationCount > 0)
+	{
+		u8 tmp[itemSize];
+		memcpy(tmp, src + itemSize * (itemCount - 1), 1);
+
+		memcpy(dest + itemSize, src, itemSize * (itemCount - 1));
+		memcpy(dest, tmp, 1);
+		--rotationCount;
+	}
+}
+
+// NOTE: Copy src rotated rotationCount times to the left into dest.
+//       If rotationCount is 0, just copy src into dest (if they are not the same).
+// IMPORTANT: Do not forget to either use strlen or to count one less
+// when calling this on a nul-terminated string.
+void rotateLeftArray(void *dest, void *src, size_t itemSize, size_t itemCount,
+					 size_t rotationCount = 1)
+{
+	if (rotationCount >= itemCount)
+	{
+		rotationCount %= itemCount;
+	}
+	
+	if ((rotationCount == 0) && (src != dest))
+	{
+		memcpy(dest, src, itemSize * itemCount);
+	}
+
+	if (rotationCount > 0)
+	{
+		u8 tmp[itemSize];
+		memcpy(tmp, src, 1);
+
+		memcpy(dest, src + itemSize, itemSize * (itemCount - 1));
+		memcpy(dest + itemSize * (itemCount - 1), tmp, 1);
+		--rotationCount;
+	}
+
+	src = dest;
+	while (rotationCount > 0)
+	{
+		u8 tmp[itemSize];
+		memcpy(tmp, src, 1);
+
+		memcpy(dest, src + itemSize, itemSize * (itemCount - 1));
+		memcpy(dest + itemSize * (itemCount - 1), tmp, 1);
+		--rotationCount;
+	}
+}
+
+// Debug
+#if EF_DEBUG
+#define ASSERT(expression)	do										\
+	{																\
+		if(!(expression))											\
+		{															\
+			fprintf(stderr, "ASSERT FAILED : '%s', %s line %d.\n",	\
+					#expression, __FILE__, __LINE__);				\
+			*(int *)NULL = 0;										\
+		}															\
+	}while (0)
+	
+#else
+#define ASSERT(expression)
+#endif
+
+#endif /* EF_UTILS_H */
---
+++ code/main.cpp
@@ -0,0 +1,526 @@
+#include "ef_utils.h"
+#include <unistd.h>
+#include <sys/wait.h>
+#include <pwd.h>
+#include <string.h>
+#include <getopt.h>
+
+// TODO: - Allow functions in .bashrc to be used as well.
+//       - Add content of directories recursively.
+//       - Make sure only one command is associated with a given
+//         extension.
+//       - Differentiate DIRECTORIES and files. (DIR, FILE)?
+//         What to do with directories then?
+//       - Allow more powerful syntax.
+
+#define ME "xopen"
+
+#define MAJOR_VERSION 0
+#define MINOR_VERSION 2
+
+#define VERSION TO_STRING(JOIN3(MAJOR_VERSION, ., MINOR_VERSION))
+
+enum OptionFlag
+{
+	OptionFlag_NONE		= 0,
+	OptionFlag_WHICH	= 1 << 1,
+};
+
+struct Instruction
+{
+	char command[255];
+	char *arguments[255];
+	
+	int commandLength;
+	int argumentCount;
+};
+
+static char usage[] =
+{
+	"Usage: "
+	ME
+	" FILE [FILE ...] [OPTION ...]\n\n"
+	"Execute a predefined command based on given files' extension.\n\n"
+	"Syntax of config file is:\n"
+	"CMD - EXTENSION [EXTENSION ...]\n\n"
+	"(EXTENSION is dot-less: 'pdf' not '.pdf')\n\n"
+	"If a line does not have any extension, it's the default command\n"
+	"(used when no other line matches).\n"
+	"In that case, '-' can be omitted.\n\n"
+	"Example:\n"
+	"evince - pdf\n"
+	"mpv - mp4 mkv\n"
+	"emacs\n\n"
+	"This will execute 'evince' on '.pdf' files, mpv on '.mp4' and '.mkv'\n"
+	"and emacs on everything else.\n\n"
+	"Options:\n"
+	"      --help        Show this (hopefully) helpful message.\n"
+	"      --version     Show this program's version.\n"
+	"  -w, --which       Show which command would be executed on each given file."
+};
+
+// NOTE: This part can be reused.
+static char version[] =
+{
+	ME
+	" "
+	VERSION
+	"\n\n"
+	"Copyright © 2016 Barbu\n"
+	"This work is free. You can redistribute it and/or modify it under the\n"
+	"terms of the Do What The Fuck You Want To Public License, Version 2,\n"
+	"as published by Sam Hocevar. See http://www.wtfpl.net/ for more details."
+};
+
+
+// Copy output of pipe_fd to buffer. Remove trailing newline if any.
+static inline int copyOutput(int pipe_fd[2], char *buffer, int bufferSize)
+{
+	close(pipe_fd[1]);
+
+	int bytesRead = read(pipe_fd[0], buffer, bufferSize - 1);
+
+	close(pipe_fd[0]);
+
+	if (*(buffer + bytesRead - 1) == '\n')
+	{
+		*(buffer + bytesRead - 1) = '\0';
+	}
+
+	return bytesRead;
+}
+
+// TODO: Flush pipes.
+// NOTE: This part can be reused.
+/* Exec command with args (commandPath is absolute, args[0] must be
+   the command name).
+   Store child's stdout in stdoutBuffer (if any).
+   Store parent or child's stderr in stderrBuffer (if any).
+   Store child's status code in statusCode if not in background.
+ 
+   Return < 0 if error on parent's part.
+          > 0 if error on child's part.
+          = 0 otherwhise.
+*/
+static int childExec(char *commandPath, char *args[],
+					 char *stdoutBuffer = NULL, int stdoutBufferSize = 0,
+					 char *stderrBuffer = NULL, int stderrBufferSize = 0,
+					 int *statusCode = NULL, b32 inBackground = false)
+{
+	// NOTE: Using macros for this is totally unnecessary.
+	//       But it's fun!
+#define PIPE_STREAM(stream, code) do									\
+	{																	\
+		if (JOIN(stream, Buffer))										\
+		{																\
+			if (pipe(JOIN(pipe_, stream)) == -1)						\
+			{															\
+				if (stderrBuffer)										\
+				{														\
+					char buffer[255];									\
+					sprintf(buffer, "%s: unable to create %s pipe.\n", ME, TO_STRING(stream)); \
+					strncpy(stderrBuffer, buffer, stderrBufferSize);	\
+				}														\
+				return code;											\
+			}															\
+		}																\
+	} while(0)
+
+#define DUP_STREAM(stream, alias) do				\
+	{												\
+		if (JOIN(stream, Buffer))					\
+		{											\
+			dup2(JOIN(pipe_, stream)[1], alias);	\
+			close(JOIN(pipe_, stream)[0]);			\
+			close(JOIN(pipe_, stream)[1]);			\
+		}											\
+	} while(0)
+
+#define COPY_STREAM(stream) do											\
+	{																	\
+		if (JOIN(stream, Buffer))										\
+		{																\
+			copyOutput(JOIN(pipe_, stream), JOIN(stream, Buffer), JOIN(stream, BufferSize)); \
+		}																\
+	} while(0)
+	
+	int pipe_stdout[2],
+		pipe_stderr[2];
+
+	PIPE_STREAM(stderr, -1);
+	PIPE_STREAM(stdout, -2);
+
+	switch(fork())
+	{
+		case -1:
+		{
+			if (stderrBuffer)
+			{
+				char buffer[255];
+				sprintf(buffer, "%s: unable to start child process.\n", ME);
+				strncpy(stderrBuffer, buffer, stderrBufferSize);
+			}
+
+			return -3;
+		}
+		case 0:
+		{
+			DUP_STREAM(stderr, STDERR_FILENO);
+			DUP_STREAM(stdout, STDOUT_FILENO);
+			
+			execv(commandPath, args);
+			
+			char buffer[255];
+			sprintf(buffer, "%s: failed to execute %s.\n", ME, args[0]);
+			perror(buffer);
+			
+			return 1;
+		}
+		default:
+		{
+			COPY_STREAM(stderr);
+			COPY_STREAM(stdout);
+
+			if (!inBackground)
+			{
+				wait(statusCode);
+			}
+
+			break;
+		}
+	}
+
+#undef PIPE_STREAM
+#undef DUP_STREAM
+#undef COPY_STREAM
+	
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	// NOTE: This part can be reused.
+	char configFile[255];
+	char *homeDir = NULL;
+	
+	if ((homeDir = getenv("XDG_CONFIG_HOME")) != NULL)
+	{
+		sprintf(configFile, "%s/%s.conf", homeDir, ME);
+	}
+	else if (((homeDir = getenv("HOME")) != NULL) ||
+			 ((homeDir = getpwuid(getuid())->pw_dir) != NULL))
+	{
+		sprintf(configFile, "%s/.config/%s.conf", homeDir, ME);
+	}
+	else
+	{
+		char buffer[255];
+		sprintf(buffer, "%s: could not create config file: no home directory found.\n", ME);
+		fprintf(stderr, buffer);
+
+		return -1;
+	}
+
+	FILE *handle = fopen(configFile, "a+");
+
+	if (handle == NULL)
+	{
+		char buffer[255];
+		sprintf(buffer, "%s: could not read config file '%s'.\n", ME, configFile);
+		fprintf(stderr, buffer);
+
+		return -2;
+	}
+	
+	fclose(handle);
+
+	Instruction allInstructions[255] = {};
+	int instructionCount = 0;
+	
+	char **allFiles;
+	int fileCount = 0;
+
+	int helpFlag = 0,
+		versionFlag = 0;
+	
+	i32 optionFlags = OptionFlag_NONE;
+
+	static struct option longOptions[] =
+		{
+			{"help", no_argument, &helpFlag, 1},
+			{"version", no_argument, &versionFlag, 1},
+			{"which", no_argument, 0, 'w'},
+			{0, 0, 0, 0}
+		};
+			
+	int c;
+	
+	for(;;)
+	{
+		int optionIndex = 0;
+		c = getopt_long(argc, argv, "w", longOptions, &optionIndex);
+
+		if (c == -1)
+		{
+			break;
+		}
+			
+		switch (c)
+		{
+			case 0:
+			{
+				if (longOptions[optionIndex].flag != 0)
+				{
+					break;
+				}
+				// printf("option %s\n", longOptions[optionIndex].name);
+				// return 0;
+			}
+			case 'w':
+			{
+				optionFlags |= OptionFlag_WHICH;
+				break;
+			}
+			default:
+			{
+				break;
+			}
+		}
+	}
+
+	if (helpFlag)
+	{
+		printf("%s\n", usage);
+		return 0;
+	}
+
+	if (versionFlag)
+	{
+		printf("%s\n", version);
+		return 0;
+	}	
+
+	fileCount = argc - optind;
+
+	if (fileCount <= 0)
+	{
+		char buffer[255];
+		sprintf(buffer, "%s: no file given.\n", ME);
+		fprintf(stderr, buffer);
+
+		return 1;
+	}
+	
+	allFiles = argv + optind;
+
+	for (int i = 0; i < fileCount; ++i)
+	{
+		char *file = allFiles[i];
+		char *extension = file;
+			
+		while (*extension && (*extension++) != '.');
+
+		// FIXME: Do not assume ouput is less than 255 chars.
+		char line[255] = {};
+		char errorBuffer[255] = {};
+		int statusCode = 0;
+		
+		char *command = line;
+		int commandLength = 0;
+
+		// NOTE: Search extension in config file.
+		//       If found, add file to associated command.
+		if (*extension)
+		{
+			// NOTE: Extensions are separated by spaces or are at the
+			//       end of a line.
+			char regexp[255];
+			sprintf(regexp, " %s( |$)", extension);
+			
+			char *grepArgs[] =
+				{
+					"grep",
+					"--extended-regexp",
+					regexp,
+					configFile,
+					NULL
+				};
+			
+			int status = childExec("/bin/grep", grepArgs,
+								   line, ARRAY_SIZE(line),
+								   errorBuffer, ARRAY_SIZE(errorBuffer),
+								   &statusCode);
+
+			if (status == 0)
+			{
+				if (statusCode == 0)
+				{
+					// NOTE: Syntax is CMD - EXTENSION [EXTENSION ...].
+					char *commandEnd = strstr(command, " - ");
+
+					if (commandEnd == NULL)
+					{
+						char buffer[255];
+						sprintf(buffer, "%s: invalid configuration for extension '%s'.\n",
+								ME, extension);
+						fprintf(stderr, buffer);
+
+						return -3;
+					}
+
+					// Remove trailing whitespaces.
+					while (*commandEnd == ' ')
+					{
+						--commandEnd;
+					}
+							
+					commandLength = commandEnd - line + 1;
+				}
+			}
+			else if (status > 0)
+			{
+				fprintf(stderr, errorBuffer);
+			}
+		}
+
+		// NOTE: Default command if no extension or no associated
+		//       command found.
+		if (!(*extension) ||
+			!(*command))
+		{
+			char *grepArgs[] =
+				{
+					"grep",
+					"--invert-match",
+					" - ",
+					configFile,
+					NULL
+				};
+			
+			int status = childExec("/bin/grep", grepArgs,
+								   line, ARRAY_SIZE(line),
+								   errorBuffer, ARRAY_SIZE(errorBuffer),
+								   &statusCode);
+				
+			if (status == 0)
+			{
+				if (statusCode == 0)
+				{
+					char *commandEnd = line + strlen(line) - 1;
+						
+					// Remove trailing whitespaces.
+					while (*commandEnd == ' ')
+					{
+						--commandEnd;
+					}
+						
+					commandLength = commandEnd - line + 1;
+				}
+			}
+			else if (status > 0)
+			{
+				fprintf(stderr, errorBuffer);
+			}
+		}
+
+		// TODO?: Keep separate error message or group by extension?
+		if (!(*command))
+		{
+			char buffer[255];
+			sprintf(buffer, "%s: %s: no command specified for extension '%s'.\n",
+					ME, file, extension);
+			fprintf(stderr, buffer);
+		}
+		else
+		{
+			b32 found = false;
+					
+			FOR_EACH(Instruction, instruction, allInstructions)
+			{
+				if (instruction->command &&
+					(instruction->commandLength == commandLength) &&
+					(strncmp(instruction->command, command, commandLength) == 0))
+				{
+					instruction->arguments[instruction->argumentCount++] = file;
+					found = true;
+					break;
+				}
+			}
+
+			if (!found)
+			{
+				Instruction *instruction = allInstructions + instructionCount++;
+						
+				instruction->commandLength = commandLength;
+				strncpy(instruction->command, command, commandLength);
+				instruction->arguments[instruction->argumentCount++] = file;
+			}
+		}
+	}
+
+	for (int i = 0; i < instructionCount; ++i)
+	{
+		Instruction instruction = allInstructions[i];
+
+		char *whichArgs[] =
+			{
+				"which",
+				instruction.command,
+				NULL
+			};
+			
+		char commandPath[255] = {};
+		int statusCode = 0;
+
+		// FIXME: Find a way to use shell functions (at least
+		//        those defined in ~/.bashrc)...
+		int status = childExec("/usr/bin/which", whichArgs,
+							   commandPath, ARRAY_SIZE(commandPath),
+							   NULL, 0,
+							   &statusCode);
+
+		if (status != 0)
+		{		
+			continue;
+		}
+
+		if (optionFlags & OptionFlag_WHICH)
+		{
+			printf("%s ", instruction.command);
+			
+			if (statusCode != 0)
+			{		
+				printf("(Unknown)");
+			}
+			else
+			{
+				printf("(%s)", commandPath);
+			}
+			
+			PRINT_N_ARRAY("\n\t%s", "", instruction.arguments, instruction.argumentCount);
+			printf("\n\n");
+		}
+		else
+		{
+			if (statusCode != 0)
+			{
+				char buffer[255];
+				sprintf(buffer, "%s: unknown command %s.\n", ME, instruction.command);
+				fprintf(stderr, buffer);
+
+				continue;
+			}
+
+			// + 2: command name + NULL. 
+			char *commandArgs[ARRAY_SIZE(((Instruction *) 0)->arguments) + 2] = {};
+
+			commandArgs[0] = instruction.command;
+			memcpy(commandArgs + 1, instruction.arguments, instruction.argumentCount * sizeof(char *));
+
+			status = childExec(commandPath, commandArgs,
+			                   NULL, 0, NULL, 0, NULL, true);
+		}
+	}
+
+	return 0;
+}
